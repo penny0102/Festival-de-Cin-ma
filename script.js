25adb009d60560cfa88233f7e9a7f63bc5f59730
miraimo <lellymohammed2006@gmail.com>
2026-04-21 10:06:45 +0100

    update java script code

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
-"use strict";
-const moviesList = document.getElementById('movies-list');
-async function main() {
+const moviesList = document.getElementById('movies-list')
+
+
+async function getDataFromApi(){
     const api = `https://api.tvmaze.com/shows?page=${randomNumber()}`;
-    try {
-        let respons = await fetch(api);
-        if (!respons.ok) {
-            console.log(respons.status);
-            return;
-        }
-        let json_data = await respons.json();
-        json_data.slice(0, 6).forEach(movie => {
-            moviesList.innerHTML += addHtmlCode(movie.image.medium, movie.name, movie.summary);
-        });
-    }
-    catch (err) {
-        console.log(err);
+    try{
+      let result = await fetch(api);
+
+        if(!result.ok) {
+        console.log(result.status);
+        return;
+      }
+
+      let jsonData = await result.json();
+      jsonData.slice(0, 6).forEach(movie => {
+        moviesList.innerHTML += movieIten(movie.image.medium, movie.name, movie.summary); 
+        
+      })
+    }catch (err){
+      console.log(err);    
     }
 }
-function addHtmlCode(img, title, desc) {
-    let htmlCode = `
+
+function movieIten(srcImg, title, desc){
+    let  html_code = `
     <div class="iten">
         <div class="card">
           <div class="card-img">
-            <img src="${img}" alt="image" />
+            <img src="${srcImg}" alt="image" />
           </div>
 
           <div class="card-body">
-            <h3>${title}:</h3>
+            <h1>${title}:</h1>
             <p>${desc}.</p>
           </div>
         </div>
       </div>
-    `;
-    return htmlCode;
+    `
+    return html_code;
+
 }
-function randomNumber() {
+
+
+function randomNumber(){
     return String(Math.floor(Math.random() * 11));
 }
 
-main();
+
+getDataFromApi();
